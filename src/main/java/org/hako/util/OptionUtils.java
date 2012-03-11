de1e09e2dd2f28c181a58ade2e491b929680fa35
xnnyygn <xnnyygn@gmail.com>
2012-03-11 14:19:17 +0800

    fix after merge

--- src/main/java/org/hako/util/OptionUtils.java
+++ src/main/java/org/hako/util/OptionUtils.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.hako.dao.util;
+package org.hako.util;
 
 import org.hako.Some;
 
